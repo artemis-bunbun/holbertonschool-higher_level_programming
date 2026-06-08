7429667027dbb3dfbf85f58cd495b16b06df18a5
artemis-bunbun <starfruitworld@gmail.com>
2026-06-08 16:01:24 +0200

    feat: creation: task 3

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""List states matching a user-provided name from MySQL database."""
+import MySQLdb
+import sys
+
+
+def main() -> None:
+    """Print rows from `states` where name matches the supplied argument."""
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    searched_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name,
+        charset="utf8",
+    )
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(searched_name)
+    cursor.execute(query)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
