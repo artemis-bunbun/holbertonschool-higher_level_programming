a2f2ca092e3aaaaea942bd218663d22af5f115e5
artemis-bunbun <starfruitworld@gmail.com>
2026-06-11 09:35:18 +0200

    feat: creation: task 11 orm model state my get

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Prints the State id for a given state name from hbtn_0e_6_usa."""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main() -> None:
+    """Search for a State by name and print its id, or Not found."""
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(user, password, db_name),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
